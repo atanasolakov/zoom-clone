e4450aa5adb8a93ec6398e32907ad79a0dc020e6
aSolakov <asolakov@imm.dev>
2024-06-08 12:22:37 +0300

    Meeeting modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,17 +3,24 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
 
 const MeetingTypeList = () => {
     const router = useRouter()
-    const [meetingState,setMeetingState] = useState<'IsScheduleMeeting ' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
-  return (
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >(undefined);
+
+   const createMeeting = () => {
+
+   }
+    return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start Instant Meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -21,7 +28,7 @@ const MeetingTypeList = () => {
         title="Schedule Meeting"
         description="Plan meeting"
         className="bg-blue-1"
-        handleClick={() => setMeetingState("IsScheduleMeeting ")}
+        handleClick={() => setMeetingState("isScheduleMeeting")}
       />
       <HomeCard
         img="/icons/recordings.svg"
@@ -37,6 +44,14 @@ const MeetingTypeList = () => {
         className="bg-yellow-1"
         handleClick={() => router.push("recordings")}
       />
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={ () => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+       />
     </section>
   );
 }
